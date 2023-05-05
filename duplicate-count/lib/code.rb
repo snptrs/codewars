03b69d19b8232a6902014eb917ddd627ef4e2e42
Sean Peters <sp@snptrs.com>
2023-05-05 11:46:50 +0100

    Added some alternate solutions to investigate

--- duplicate-count/lib/code.rb
+++ duplicate-count/lib/code.rb
@@ -3,3 +3,16 @@ def duplicate_count(text)
   text.downcase.split("").uniq.each { |i| dupes += 1 if text.downcase.count(i) > 1 }
   dupes
 end
+
+# This is an interesting solution. Don't really undersdtand what's going on with it
+# [INVESTIGATE]
+# def duplicate_count(text)
+#   ('a'..'z').count { |c| text.downcase.count(c) > 1 }
+# end
+
+# Another good one
+# Didn't realise you could apply count directly to an array or range
+# def duplicate_count(text)
+# 	arr = text.downcase.split("")
+#   arr.uniq.count { |n| arr.count(n) > 1 }
+# end
